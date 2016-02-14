a4fafc0c42fa70b09007551690706e145f1b5983
John Ryan <john.ryan@cardinalhealth.com>
2016-02-13 21:59:55 -0500

    MIRAGE!!!  ok, so I tested around this flaw in the data mocking

--- tests/acceptance/soda-test.js
+++ tests/acceptance/soda-test.js
@@ -1,4 +1,5 @@
 import {
+  test,
   skip
 }
 from 'qunit';
@@ -17,14 +18,39 @@ skip('when creating a soda (mirage issue)',
   // This fails in mirage as for some reason the sodas model does not autoupdate on save.
   // manually verified that this feature function once I integrated firebase.
   function(assert) {
-    assert.expect(2);
+    assert.expect(1);
 
     click('#createSodaLink');
-    fillIn('#sodaNameInput input', 'My new soda');
-    click('#createSoda');
-
     andThen(function() {
-      assert.ok($('h3:contains("My new soda")').length > 0,
-        'it displays "My new soda"');
+      fillIn('#sodaNameInput input', 'My new soda');
+      click('#createSoda');
+      andThen(function() {
+        assert.ok($('h3:contains("My new soda")').length > 0,
+          'it displays "My new soda"');
+      });
     });
+
+  });
+
+test('when creating a soda',
+  function(assert) {
+    assert.expect(1);
+
+    click('#createSodaLink');
+    andThen(function() {
+      fillIn('#sodaNameInput input', 'My new soda');
+      click('#createSoda');
+      andThen(function() {
+        click('md-toolbar button:nth-child(3) a');
+        andThen(function() {
+          click('md-toolbar button:nth-child(1) a');
+          andThen(function() {
+            assert.ok($('h3:contains("My new soda")').length >
+              0,
+              'it displays "My new soda"');
+          });
+        });
+      });
+    });
+
   });
