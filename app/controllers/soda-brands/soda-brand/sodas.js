8f718a3ed31f6c4e2d5c41ca054960b58fa7646b
John Ryan <john.ryan@cardinalhealth.com>
2016-02-13 23:46:14 -0500

    Slight refactor

--- app/controllers/soda-brands/soda-brand/sodas.js
+++ app/controllers/soda-brands/soda-brand/sodas.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sodaBrandController: Ember.inject.controller('sodaBrands.sodaBrand'),
+  sodaBrandModel: Ember.computed.reads('sodaBrandController.model'),
   actions: {
     goToSoda: function(soda) {
       this.transitionToRoute('sodaBrands.sodaBrand.soda', soda.id);
