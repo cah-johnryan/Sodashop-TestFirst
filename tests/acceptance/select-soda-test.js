a88488fd6e9a949a23e29bbd9074a1cfdb27b73e
John Ryan <john.ryan@cardinalhealth.com>
2016-02-25 17:26:37 -0500

    expanding tests

--- tests/acceptance/select-soda-test.js
+++ tests/acceptance/select-soda-test.js
@@ -81,6 +81,28 @@ test('When selecting a soda that is sold out', function(assert) {
   });
 });
 
+test('When updating the quantity of a soda that is sold out', function(assert) {
+  visit('/1/sodas');
+  andThen(function() {
+    assert.equal($('md-list-item:nth-child(7) h4').text(),
+      'SOLD OUT', 'the product now states that it is sold out');
+    click('md-list-item:nth-child(7) a');
+    andThen(function() {
+      click('button[action="beginEditSoda"]');
+      andThen(function() {
+        fillIn('#sodaEditQuantity input', '1');
+        click('button[action="updateSoda"]');
+        andThen(function() {
+          assert.equal($('md-list-item:nth-child(7) h4').text(),
+            '$0.50',
+            'the product now states its price (not sold out)'
+          );
+        });
+      });
+    });
+  });
+});
+
 test('When selecting the last soda', function(assert) {
   click('#quarter');
   click('#quarter');
