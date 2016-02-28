301d2922c419736510e936380942d23bd2a8714b
John Ryan <john.ryan@cardinalhealth.com>
2016-02-28 00:45:51 -0500

    Component expansion

---
+++ tests/integration/components/soda-details-test.js
@@ -0,0 +1,29 @@
+import {
+  moduleForComponent, test
+}
+from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('soda-details', 'Integration | Component | soda details', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.ok(true);
+  //
+  // // Set any properties with this.set('myProperty', 'value');
+  // // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  //
+  // this.render(hbs `{{soda-details}}`);
+  //
+  // assert.equal(this.$().text().trim(), '');
+  //
+  // // Template block usage:" + EOL +
+  // this.render(hbs `
+  //   {{#soda-details}}
+  //     template block text
+  //   {{/soda-details}}
+  // `);
+  //
+  // assert.equal(this.$().text().trim(), 'template block text');
+});
