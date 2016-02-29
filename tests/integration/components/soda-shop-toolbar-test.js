4c2f884b666a80fac315710f4fae4dbbc9089133
John Ryan <john.ryan@cardinalhealth.com>
2016-02-28 21:10:41 -0500

    toolbar component

---
+++ tests/integration/components/soda-shop-toolbar-test.js
@@ -0,0 +1,30 @@
+import {
+  moduleForComponent, test
+}
+from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('soda-shop-toolbar',
+  'Integration | Component | soda shop toolbar', {
+    integration: true
+  });
+
+test('it renders', function(assert) {
+  assert.ok(true);
+  //
+  // // Set any properties with this.set('myProperty', 'value');
+  // // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  //
+  // this.render(hbs`{{soda-shop-toolbar}}`);
+  //
+  // assert.equal(this.$().text().trim(), '');
+  //
+  // // Template block usage:" + EOL +
+  // this.render(hbs`
+  //   {{#soda-shop-toolbar}}
+  //     template block text
+  //   {{/soda-shop-toolbar}}
+  // `);
+  //
+  // assert.equal(this.$().text().trim(), 'template block text');
+});
