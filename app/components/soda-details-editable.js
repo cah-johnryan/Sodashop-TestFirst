cdf85b58c35ae12d180f8ab71eb08c8f853e3388
John Ryan <john.ryan@cardinalhealth.com>
2016-04-05 22:08:30 -0400

    Client side validation

--- app/components/soda-details-editable.js
+++ app/components/soda-details-editable.js
@@ -1,6 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  costValidation: {
+    'errorMessage': 'A cost is required with a value of zero or greater which can be broken down to nickles',
+    'isError': (inputValue) => {
+      var pattern = /^[0-9]\d*(\.\d+)?$/;
+      return !canBeBrokenDownIntoNickels(inputValue) || !pattern.test(inputValue);
+
+      function canBeBrokenDownIntoNickels(inputValue) {
+        return parseInt(Number(inputValue) * 100) % 5 === 0;
+      }
+    }
+  },
+  quantityValidation: {
+    'errorMessage': 'A quantity is required with a integer value of zero or greater',
+    'isError': (inputValue) => {
+      var pattern = /^[0-9]*$/;
+      return !pattern.test(inputValue);
+    }
+  },
   actions: {
     cancelEditSoda() {
         this.get('returnToSodaListing')();
