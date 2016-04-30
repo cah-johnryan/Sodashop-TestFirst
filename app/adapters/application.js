c7cb9ff4a90decd71ff9c4198b2784aa0e569c7d
John Ryan <john.ryan@cardinalhealth.com>
2016-04-30 19:30:29 -0400

    Reintroduced firebase

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,19 +1,22 @@
-// // Emberfire adapter settings
-// import Ember from 'ember';
-// import FirebaseAdapter from 'emberfire/adapters/firebase';
-//
-// const {
-//   inject
-// } = Ember;
-//
-// export default FirebaseAdapter.extend({
-//   firebase: inject.service(),
-// });
-
-// // Standard non-Emberfire, mirage friendly adapter settings
-import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
+// // Standard non-Emberfire, mirage friendly adapter settings
+
+// import DS from 'ember-data';
+// export default DS.RESTAdapter.extend(DataAdapterMixin, {
+//   authorizer: 'authorizer:oauth2'
+// });
+
+
+// Emberfire adapter settings
+import Ember from 'ember';
+import FirebaseAdapter from 'emberfire/adapters/firebase';
+
+const {
+  inject
+} = Ember;
+
+export default FirebaseAdapter.extend(DataAdapterMixin, {
+  firebase: inject.service(),
   authorizer: 'authorizer:oauth2'
 });
