a2ab52f5da849d56732582c7afdbcf4951cd5fb9
John Ryan <john.ryan@cardinalhealth.com>
2016-10-31 08:21:05 -0400

    wip on tests

--- tests/acceptance/soda-brand-admin-test.js
+++ tests/acceptance/soda-brand-admin-test.js
@@ -1,19 +1,13 @@
-import {
-  test
-}
-from 'qunit';
-import moduleForAcceptance from
-  'sodashop-test-first/tests/helpers/module-for-acceptance';
+import { test } from 'qunit';
+import Application from '../../app';
+import moduleForAcceptance from 'sodashop-test-first/tests/helpers/module-for-acceptance';
+import { authenticateSession } from 'sodashop-test-first/tests/helpers/ember-simple-auth';
 
 moduleForAcceptance('Acceptance | soda brand admin', {
   beforeEach() {
     server.loadFixtures();
-    andThen(function() {
-      if (($('#login').length !== 0)) {
-        click('#login');
-      }
-      visit('/');
-    });
+    visit('/');
+    authenticateSession(application, {'authorizationCode': 'dummyValue'});
   },
   afterEach() {}
 });
@@ -29,15 +23,17 @@ test('the create soda brands icon is present', function(assert) {
 test('when creating a soda brand',
   function(assert) {
     click('#createSodaBrandLink');
-    fillIn('#brandNameInput input', 'My new soda brand');
-    click('#createSodaBrand');
     andThen(function() {
-      let result = $('.md-toolbar-tools span').filter(function() {
-        return $(this).text() === 'My new soda brand';
+      fillIn('#brandNameInput input', 'My new soda brand');
+      click('#createSodaBrand');
+      andThen(function() {
+        let result = $('.md-toolbar-tools span').filter(function() {
+          return $(this).text() === 'My new soda brand';
+        });
+        assert.ok(result.length > 0, 'the toolbar shows the new soda brand');
+        assert.ok($('.sodaImage').length > 0,
+          'the image for the soda brand is shown');
       });
-      assert.ok(result.length > 0, 'the toolbar shows the new soda brand');
-      assert.ok($('.sodaImage').length > 0,
-        'the image for the soda brand is shown');
     });
   }
 );
