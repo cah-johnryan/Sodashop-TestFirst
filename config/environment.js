c42bde0d02b3f82541cc3f5f1cb1ac8b5b0c0798
John Ryan <john.ryan@cardinalhealth.com>
2019-01-29 19:00:23 -0500

    ember-cli-update --to 2.18.2

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+'use strict';
 
 module.exports = function(environment) {
   var ENV = {
@@ -24,6 +24,10 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
@@ -65,7 +69,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     ENV['simple-auth'] = {
@@ -77,6 +80,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
     ENV['ember-cli-mirage'] = {
       enabled: true
     };
